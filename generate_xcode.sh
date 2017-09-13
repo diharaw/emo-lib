771248e095ba52f9ed4e0758ce1ee5d277bacbdd
Dihara Wijetunga <dihara.w@fidenz.com>
2017-09-13 15:05:19 +0530

    Updated CMakeLists

--- generate_xcode.sh
+++ generate_xcode.sh
@@ -2,4 +2,4 @@ rm -rf build
 mkdir build
 cd build 
 
-cmake -DBUILD_SHARED_LIBS=OFF -DDISABLE_DEVICE_HOST_UNIFIED_MEMORY=ON -DBUILD_docs=OFF -DBUILD_python=OFF -DBUILD_python_layer=OFF -DBLAS="open" -DOpenCV_DIR="/Users/diharaw/opencv/build" -DCMAKE_PREFIX_PATH="../external/viennacl-dev" -G Xcode ..
+cmake -DBUILD_SHARED_LIBS=OFF -DBUILD_LIBXTRACT_SAMPLE=OFF -DDISABLE_DEVICE_HOST_UNIFIED_MEMORY=ON -DBUILD_docs=OFF -DBUILD_python=OFF -DBUILD_python_layer=OFF -DBLAS="open" -DOpenCV_DIR="/Users/diharaw/opencv/build" -DCMAKE_PREFIX_PATH="../external/viennacl-dev" -G Xcode ..
