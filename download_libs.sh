d2e8fede01eca04be27d0047178eb1d93c496a53
Dihara Wijetunga <dihara.w@fidenz.com>
2017-09-13 14:52:12 +0530

    Updated CMakeLists

---
+++ download_libs.sh
@@ -0,0 +1,10 @@
+cd external
+
+rm -rf caffe
+git clone https://github.com/BVLC/caffe.git
+cd caffe
+git checkout opencl
+cd ..
+
+rm -rf viennacl-dev
+git clone https://github.com/viennacl/viennacl-dev.git
